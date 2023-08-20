e56017e24b348ac154fe9587f055ce9af1bf7483
Chandra <feechanz27@gmail.com>
2023-08-20 21:26:58 +0900

    fix start script

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ubuntu
-java -jar main-app.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar /home/ubuntu/main-app/build/libs/main-app.jar > /dev/null 2> /dev/null < /dev/null &
