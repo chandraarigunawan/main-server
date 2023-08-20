959baf31152e1c11791e76b072097c45159f50df
Chandra <feechanz27@gmail.com>
2023-08-20 11:34:37 +0900

    add unit test

---
+++ main-web/src/test/java/com/main/app/web/controller/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.main.app.web.controller;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloControllerTest {
+  private HelloController target = new HelloController();
+
+  @Test
+  public void testHello() {
+    String hello = target.hello();
+    assertThat(hello).isEqualTo("hello world");
+  }
+}
