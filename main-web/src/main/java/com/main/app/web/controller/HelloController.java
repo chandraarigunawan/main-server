8a38a17585fbce3cb0f68c2ed74d7ea58e6c7a20
Chandra Ari Gunawan <feechanz27@gmail.com>
2023-08-24 21:22:37 +0900

    Update HelloController.java

--- main-web/src/main/java/com/main/app/web/controller/HelloController.java
+++ main-web/src/main/java/com/main/app/web/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String hello() {
-    return "hello world API";
+    return "hello world API versi 2";
   }
 }
